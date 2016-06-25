0c65df4f97443f126ac7aac348188af7f0f1f3ed
Vaibhav Bali <vibsbali@gmail.com>
2016-06-25 11:40:16 +1000

    Added a graph class and Vertex class

---
+++ Graphs/UndirectedGraph.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Graphs
+{
+    public class UndirectedGraph<T>
+    {
+        public int NumberOfVertices { get; private set; }
+        public int NumberOfEdges { get; private set; }
+
+        private List<Vertex<T>> AdjencyList { get; set; }
+
+        public UndirectedGraph(int numberOfVertices)
+        {
+            AdjencyList = new List<Vertex<T>>(numberOfVertices);
+        }
+
+        public void AddEdge(Vertex<T> firstVertex, Vertex<T> connectingVertex)
+        {
+            
+        }
+
+        public IEnumerable<Vertex<T>> GetAdjencyVertices(int vertex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Vertex<T>> GetAdjencyVertices(Vertex<T> vertex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ Graphs/Vertex.cs
@@ -0,0 +1,14 @@
+﻿namespace Graphs
+{
+    public class Vertex<T>
+    {
+        public T Value { get; private set; }
+        public int Index { get; private set; }
+
+        public Vertex(int index, T value)
+        {
+            Index = index;
+            Value = value;
+        }
+    }
+}
