f2c1d77854f8c642b2b9635be11c00ed13f8b893
Vaibhav Bali <vibsbali@gmail.com>
2016-06-25 19:53:33 +1000

    Added support of Finding Path from source to destination vertex

--- AlgorithmsAndDataStructures/Program.cs
+++ AlgorithmsAndDataStructures/Program.cs
@@ -5,26 +5,45 @@ namespace AlgorithmsAndDataStructures
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            if (args == null) throw new ArgumentNullException(nameof(args));
-            var graph = new UndirectedGraph<int>(13);
+            //var graph = new UndirectedGraph<int>(13);
 
-            graph.AddEdge(new Vertex<int>(0, 0), new Vertex<int>(5, 5));
-            graph.AddEdge(new Vertex<int>(4, 4), new Vertex<int>(3, 3));
+            //graph.AddEdge(new Vertex<int>(0, 0), new Vertex<int>(5, 5));
+            //graph.AddEdge(new Vertex<int>(4, 4), new Vertex<int>(3, 3));
+            //graph.AddEdge(new Vertex<int>(0, 0), new Vertex<int>(1, 1));
+            //graph.AddEdge(new Vertex<int>(9, 9), new Vertex<int>(12, 12));
+            //graph.AddEdge(new Vertex<int>(6, 6), new Vertex<int>(4, 4));
+            //graph.AddEdge(new Vertex<int>(5, 5), new Vertex<int>(4, 4));
+            //graph.AddEdge(new Vertex<int>(0, 0), new Vertex<int>(2, 2));
+            //graph.AddEdge(new Vertex<int>(11, 11), new Vertex<int>(12, 12));
+            //graph.AddEdge(new Vertex<int>(9, 9), new Vertex<int>(10, 10));
+            //graph.AddEdge(new Vertex<int>(0, 0), new Vertex<int>(6, 6));
+            //graph.AddEdge(new Vertex<int>(7, 7), new Vertex<int>(8, 8));
+            //graph.AddEdge(new Vertex<int>(9, 9), new Vertex<int>(11, 11));
+            //graph.AddEdge(new Vertex<int>(5, 5), new Vertex<int>(3, 3));
+
+            var graph = new UndirectedGraph<int>(6);
+            graph.AddEdge(new Vertex<int>(0,0), new Vertex<int>(5, 5));
+            graph.AddEdge(new Vertex<int>(2, 2), new Vertex<int>(4, 4));
+            graph.AddEdge(new Vertex<int>(2, 2), new Vertex<int>(3, 3));
+            graph.AddEdge(new Vertex<int>(1, 1), new Vertex<int>(2, 2));
             graph.AddEdge(new Vertex<int>(0, 0), new Vertex<int>(1, 1));
-            graph.AddEdge(new Vertex<int>(9, 9), new Vertex<int>(12, 12));
-            graph.AddEdge(new Vertex<int>(6, 6), new Vertex<int>(4, 4));
-            graph.AddEdge(new Vertex<int>(5, 5), new Vertex<int>(4, 4));
+            graph.AddEdge(new Vertex<int>(3, 3), new Vertex<int>(4, 4));
+            graph.AddEdge(new Vertex<int>(3, 3), new Vertex<int>(5, 5));
             graph.AddEdge(new Vertex<int>(0, 0), new Vertex<int>(2, 2));
-            graph.AddEdge(new Vertex<int>(11, 11), new Vertex<int>(12, 12));
-            graph.AddEdge(new Vertex<int>(9, 9), new Vertex<int>(10, 10));
-            graph.AddEdge(new Vertex<int>(0, 0), new Vertex<int>(6, 6));
-            graph.AddEdge(new Vertex<int>(7, 7), new Vertex<int>(8, 8));
-            graph.AddEdge(new Vertex<int>(9, 9), new Vertex<int>(11, 11));
-            graph.AddEdge(new Vertex<int>(5, 5), new Vertex<int>(3, 3));
 
             Console.WriteLine(graph);
+
+            var dfs = new DepthFirstSearch(graph, 0);
+            
+            Console.WriteLine(dfs.Count());
+            Console.WriteLine(dfs.PathExists(5));
+
+            foreach (var i in dfs.PathTo(0))
+            {
+                Console.Write(i + " -> ");
+            }
         }
     }
 }
--- Graphs/DepthFirstSearch.cs
+++ Graphs/DepthFirstSearch.cs
@@ -1,16 +1,37 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace Graphs
 {
-    public class DepthFirstSearch<T>
+    public class DepthFirstSearch
     {
-        private readonly UndirectedGraph<T> graph;
-        private readonly int sourceVertex;
+        private bool[] marked;
+        private int count;
 
-        public DepthFirstSearch(UndirectedGraph<T> graph, int sourceVertex)
+        private int[] edgeTo;
+        private readonly int sourceVertex;
+        
+
+        public DepthFirstSearch(UndirectedGraph<int> graph, int sourceVertex)
         {
-            this.graph = graph;
+            edgeTo = new int[graph.NumberOfVertices];
+            marked = new bool[graph.NumberOfVertices];
+
             this.sourceVertex = sourceVertex;
+            Dfs(graph, sourceVertex);
+        }
+
+        private void Dfs(UndirectedGraph<int> graph, int vertexBeingVisted)
+        {
+            marked[vertexBeingVisted] = true;
+            foreach (var adjacentVertex in graph.GetAdjencyVertices(vertexBeingVisted))
+            {
+                count++;
+                if (!marked[adjacentVertex])
+                {
+                    edgeTo[adjacentVertex] = vertexBeingVisted;
+                    Dfs(graph, adjacentVertex);
+                }
+            }
         }
 
         /// <summary>
@@ -18,10 +39,39 @@ namespace Graphs
         /// </summary>
         /// <param name="vertex"></param>
         /// <returns></returns>
-        public bool Marked(int vertex)
+        private bool HasVisted(int vertex)
         {
-            throw new NotImplementedException();
+            return marked[vertex];
         }
+
+        /// <summary>
+        /// A wrapper function to check, Is there a path from source vertex to a given vertex v
+        /// </summary>
+        /// <param name="vertex"></param>
+        /// <returns></returns>
+        public bool PathExists(int vertex)
+        {
+            return HasVisted(vertex);
+        }
+
+        public IEnumerable<int> PathTo(int vertex)
+        {
+            if (PathExists(vertex))
+            {
+                var path = new Stack<int>();
+                int root= vertex;
+                do
+                {
+                    path.Push(root);
+                    root = edgeTo[root];
+                } while (root != sourceVertex);
+
+                path.Push(sourceVertex);
+                return path;
+            }
+
+            return null;
+        } 
 
         /// <summary>
         /// How many vertices are connected to source vertex
@@ -29,7 +79,7 @@ namespace Graphs
         /// <returns></returns>
         public int Count()
         {
-            throw new NotImplementedException();
+            return count;
         }
     }
 }
