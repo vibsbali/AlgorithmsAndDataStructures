4c3240e5b204c3a172bb26cb3f28a3085335e249
Vaibhav Bali <vibsbali@gmail.com>
2016-06-26 19:38:12 +1000

    Code in Program.cs

--- AlgorithmsAndDataStructures/Program.cs
+++ AlgorithmsAndDataStructures/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Graphs;
 
 namespace AlgorithmsAndDataStructures
@@ -7,6 +8,7 @@ namespace AlgorithmsAndDataStructures
     {
         static void Main()
         {
+            #region Undirected Graph
             //var graph = new UndirectedGraph<int>(13);
 
             //graph.AddEdge(new Vertex<int>(0, 0), new Vertex<int>(5, 5));
@@ -32,16 +34,67 @@ namespace AlgorithmsAndDataStructures
             //graph.AddEdge(new Vertex<int>(3, 3), new Vertex<int>(4, 4));
             //graph.AddEdge(new Vertex<int>(3, 3), new Vertex<int>(5, 5));
             //graph.AddEdge(new Vertex<int>(0, 0), new Vertex<int>(2, 2));
+            #endregion
 
+            #region Directed Graph
             //var graph = new DirectedGraph(13);
 
             //var arrayOfVs = new[] { 4, 2, 3, 6, 0, 2, 11, 12, 9, 9, 8, 10, 11, 4, 3, 7, 8, 5, 0, 6, 6, 7 };
             //var arrayOfWs = new[] { 2, 3, 2, 0, 1, 0, 12, 9, 10, 11, 9, 12, 4, 3, 5, 8, 7, 4, 5, 4, 9, 6 };
 
-            var graph = new DirectedGraph(7);
+            //var graph = new DirectedGraph(7);
 
-            var arrayOfVs = new[] { 0, 0, 3, 5, 6, 1, 0, 3, 3, 6, 3 };
-            var arrayOfWs = new[] { 5, 1, 5, 2, 0, 4, 2, 6, 4, 4, 2 };
+            //var arrayOfVs = new[] { 0, 0, 3, 5, 6, 1, 0, 3, 3, 6, 3 };
+            //var arrayOfWs = new[] { 5, 1, 5, 2, 0, 4, 2, 6, 4, 4, 2 };
+
+
+            //for (int i = 0; i < arrayOfVs.Length; i++)
+            //{
+            //    graph.AddEdge(arrayOfVs[i], arrayOfWs[i]);
+            //}
+
+
+            //Console.WriteLine(graph);
+
+            //var dfs = new DirectedCycle(graph);
+
+            //if (dfs.HasCycle())
+            //{
+            //    Console.WriteLine(dfs.HasCycle());
+            //    foreach (var path in dfs.RetrieveCycle())
+            //    {
+            //        Console.Write(path + " -> ");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Graph is a DAG");
+
+            //    var dfsVertexOrdering = new DfsVertexOrdering(graph);
+
+            //    Console.WriteLine("\nPrinting Pre Order");
+            //    foreach (var df in dfsVertexOrdering.PreOrder())
+            //    {
+            //        Console.Write(df);
+            //    }
+            //    Console.WriteLine("\nPrinting Post Order");
+            //    foreach (var df in dfsVertexOrdering.PostOrder())
+            //    {
+            //        Console.Write(df);
+            //    }
+            //    Console.WriteLine("\nPrinting Reverse Post Order");
+            //    foreach (var df in dfsVertexOrdering.ReversePostOrder())
+            //    {
+            //        Console.Write(df);
+            //    }
+            //}
+            #endregion
+
+            #region KosarajuSCC
+            var graph = new DirectedGraph(13);
+
+            var arrayOfVs = new[] { 4, 2, 3, 6, 0, 2, 11, 12, 9, 9, 8, 10, 11, 4, 3, 7, 8, 5, 0, 6, 6, 7 };
+            var arrayOfWs = new[] { 2, 3, 2, 0, 1, 0, 12, 9, 10, 11, 9, 12, 4, 3, 5, 8, 7, 4, 5, 4, 9, 6 };
 
 
             for (int i = 0; i < arrayOfVs.Length; i++)
@@ -49,44 +102,21 @@ namespace AlgorithmsAndDataStructures
                 graph.AddEdge(arrayOfVs[i], arrayOfWs[i]);
             }
 
+           var kcc = new KosarajuSCC(graph);
+            Console.WriteLine(kcc.Count);
 
-            Console.WriteLine(graph);
-
-            var dfs = new DirectedCycle(graph);
-
-            if (dfs.HasCycle())
+            foreach (var @group in kcc.GetGroups())
             {
-                Console.WriteLine(dfs.HasCycle());
-                foreach (var path in dfs.RetrieveCycle())
+                Console.Write("Items in group " + @group.Key + " : ");
+                foreach (var i in @group.Value)
                 {
-                    Console.Write(path + " -> ");
+                    Console.Write(i + ",");
                 }
+                Console.WriteLine();
             }
-            else
-            {
-                Console.WriteLine("Graph is a DAG");
-
-                var dfsVertexOrdering = new DfsVertexOrdering(graph);
-
-                Console.WriteLine("\nPrinting Pre Order");
-                foreach (var df in dfsVertexOrdering.PreOrder())
-                {
-                    Console.Write(df);
-                }
-                Console.WriteLine("\nPrinting Post Order");
-                foreach (var df in dfsVertexOrdering.PostOrder())
-                {
-                    Console.Write(df);
-                }
-                Console.WriteLine("\nPrinting Reverse Post Order");
-                foreach (var df in dfsVertexOrdering.ReversePostOrder())
-                {
-                    Console.Write(df);
-                }
-            }
+#endregion
 
 
-            
         }
     }
 }
