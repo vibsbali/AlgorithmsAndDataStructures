0a5970b08318e7f945fa9c5db1c4e7f839e8397f
Vaibhav Bali <vibsbali@gmail.com>
2016-06-26 19:37:43 +1000

    Implemented Kosaraju Strongly Connected Components

---
+++ Graphs/KosarajuSCC.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    public class KosarajuSCC
+    {
+        private readonly bool[] marked;
+        public int Count { get; }
+        private readonly int[] idsOfStronglyConnectedComponents;
+
+        public KosarajuSCC(DirectedGraph graph)
+        {
+            marked = new bool[graph.NumberOfVertices];
+            idsOfStronglyConnectedComponents = new int[graph.NumberOfVertices];
+
+            DfsVertexOrdering order = new DfsVertexOrdering(graph.Reverse());
+
+            foreach (var s in order.ReversePostOrder())
+            {
+                if (!marked[s])
+                {
+                    Dfs(graph, s);
+                    Count++;
+                }
+            }
+        }
+
+        private void Dfs(DirectedGraph graph, int sourceVertex)
+        {
+            marked[sourceVertex] = true;
+            idsOfStronglyConnectedComponents[sourceVertex] = Count;
+
+            foreach (var w in graph.GetAdjacencyVertices(sourceVertex))
+            {
+                if (!marked[w])
+                {
+                    Dfs(graph, w);
+                }
+            }
+        }
+
+        public bool IsConnected(int sourceVertex, int w)
+        {
+            return idsOfStronglyConnectedComponents[sourceVertex] == idsOfStronglyConnectedComponents[w];
+        }
+
+        public int GetStronglyConnectedComponentId(int vertex)
+        {
+            return idsOfStronglyConnectedComponents[vertex];
+        }
+
+        public Dictionary<int, List<int>> GetGroups()
+        {
+            var dict = new Dictionary<int, List<int>>();
+            for (int i = 0; i < Count; i++)
+            {
+                dict.Add(i, new List<int>());
+                for (int j = 0; j < idsOfStronglyConnectedComponents.Length; j++)
+                {
+                    if (idsOfStronglyConnectedComponents[j] == i)
+                    {
+                        dict[i].Add(j);
+                    }
+                }
+            }
+
+            return dict;
+        } 
+    }
+}
