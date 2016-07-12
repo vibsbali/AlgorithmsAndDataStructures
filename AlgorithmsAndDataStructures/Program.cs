2d0d5bdf820ad46b3cdebe93e98a41a6f1d1d0dc
Vaibhav Bali <vbali@internode.on.net>
2016-07-12 15:07:34 +1000

    Implemented UnionFindQuick

--- AlgorithmsAndDataStructures/Program.cs
+++ AlgorithmsAndDataStructures/Program.cs
@@ -307,7 +307,45 @@ namespace AlgorithmsAndDataStructures
             #endregion
 
             #region dynamicConnectivityBasic
-            var dynamicConnectivity = new UnionFindBasic(10);
+            //var dynamicConnectivity = new UnionFindBasic(10);
+
+            //Console.WriteLine(dynamicConnectivity.Connect(4, 3));
+            //Console.WriteLine(dynamicConnectivity.Connect(3, 8));
+            //Console.WriteLine(dynamicConnectivity.Connect(6, 5));
+            //Console.WriteLine(dynamicConnectivity.Connect(9, 4));
+            //Console.WriteLine(dynamicConnectivity.Connect(2, 1));
+            //Console.WriteLine(dynamicConnectivity.Connect(8, 9));
+            //Console.WriteLine(dynamicConnectivity.Connect(5, 0));
+            //Console.WriteLine(dynamicConnectivity.Connect(7, 2));
+            //Console.WriteLine(dynamicConnectivity.Connect(6, 1));
+            //Console.WriteLine(dynamicConnectivity.Connect(1, 0));
+            //Console.WriteLine(dynamicConnectivity.Connect(6, 7));
+
+            //Console.WriteLine(dynamicConnectivity.IsConnected(6, 7));
+
+            #endregion
+
+            #region dynamicConnectivityWithTrees
+            //var dynamicConnectivityWithTrees = new UnionFindWithTree<int>();
+
+            //Console.WriteLine("Using Trees");
+            //Console.WriteLine(dynamicConnectivityWithTrees.Connect(4, 3));
+            //Console.WriteLine(dynamicConnectivityWithTrees.Connect(3, 8));
+            //Console.WriteLine(dynamicConnectivityWithTrees.Connect(6, 5));
+            //Console.WriteLine(dynamicConnectivityWithTrees.Connect(9, 4));
+            //Console.WriteLine(dynamicConnectivityWithTrees.Connect(2, 1));
+            //Console.WriteLine(dynamicConnectivityWithTrees.Connect(8, 9));
+            //Console.WriteLine(dynamicConnectivityWithTrees.Connect(5, 0));
+            //Console.WriteLine(dynamicConnectivityWithTrees.Connect(7, 2));
+            //Console.WriteLine(dynamicConnectivityWithTrees.Connect(6, 1));
+            //Console.WriteLine(dynamicConnectivityWithTrees.Connect(1, 0));
+            //Console.WriteLine(dynamicConnectivityWithTrees.Connect(6, 7));
+
+            //Console.WriteLine(dynamicConnectivityWithTrees.IsConnected(6, 7));
+            #endregion
+
+            #region dynamicConnectivityWithUnionFindQuick
+            var dynamicConnectivity = new UnionFindQuick(10);
 
             Console.WriteLine(dynamicConnectivity.Connect(4, 3));
             Console.WriteLine(dynamicConnectivity.Connect(3, 8));
@@ -321,27 +359,7 @@ namespace AlgorithmsAndDataStructures
             Console.WriteLine(dynamicConnectivity.Connect(1, 0));
             Console.WriteLine(dynamicConnectivity.Connect(6, 7));
 
-            Console.WriteLine(dynamicConnectivity.IsConnected(6, 7));
-
-            #endregion
-
-            #region dynamicConnectivityWithTrees
-            var dynamicConnectivityWithTrees = new UnionFindWithTree<int>();
-
-            Console.WriteLine("Using Trees");
-            Console.WriteLine(dynamicConnectivityWithTrees.Connect(4, 3));
-            Console.WriteLine(dynamicConnectivityWithTrees.Connect(3, 8));
-            Console.WriteLine(dynamicConnectivityWithTrees.Connect(6, 5));
-            Console.WriteLine(dynamicConnectivityWithTrees.Connect(9, 4));
-            Console.WriteLine(dynamicConnectivityWithTrees.Connect(2, 1));
-            Console.WriteLine(dynamicConnectivityWithTrees.Connect(8, 9));
-            Console.WriteLine(dynamicConnectivityWithTrees.Connect(5, 0));
-            Console.WriteLine(dynamicConnectivityWithTrees.Connect(7, 2));
-            Console.WriteLine(dynamicConnectivityWithTrees.Connect(6, 1));
-            Console.WriteLine(dynamicConnectivityWithTrees.Connect(1, 0));
-            Console.WriteLine(dynamicConnectivityWithTrees.Connect(6, 7));
-
-            Console.WriteLine(dynamicConnectivityWithTrees.IsConnected(6, 7));
+            
             #endregion
 
             #endregion
---
+++ FundamentalDataStructures/UnionFindQuick.cs
@@ -0,0 +1,42 @@
+﻿namespace FundamentalDataStructures
+{
+    public class UnionFindQuick
+    {
+        private readonly int[] backingArray;
+
+        public UnionFindQuick(int size)
+        {
+            backingArray = new int[size];
+            for (int i = 0; i < backingArray.Length; i++)
+            {
+                backingArray[i] = i;
+            }
+        }
+
+        private int FindRoot(int input)
+        {
+            while (input != backingArray[input])
+            {
+                input = backingArray[input];
+            }
+
+            return input;
+        }
+
+        //Find the root of second element and make root of the first element the child of it
+        public string Connect(int v1, int v2)
+        {
+            var rootForFirstElement = FindRoot(v1);
+            var rootForSecondElement = FindRoot(v2);
+            
+
+            if (rootForFirstElement == rootForSecondElement)
+            {
+                return "Already Connected";
+            }
+
+            backingArray[rootForFirstElement] = rootForSecondElement;
+            return "Connected";
+        }
+    }
+}
